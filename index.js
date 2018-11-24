363c6b46457ba1bb0a1da6fc31c098c4884e4808
nacho <nachofuentes7164@gmail.com>
2018-11-24 15:40:23 -0300

    n
    sfa

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+      var height = $(window).height();
+
+      $('#inicio').height(height);
+});
+    
+    
+
+$(document).ready(function(){
+
+      var height = $(window).height();
+
+      $('#acercade').height(height);
+});
+
+$(document).ready(function(){
+
+      var height = $(window).height();
+
+      $('#servicios').height(height);
+});
+    
